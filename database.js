39ea2a1c7535345de013d08f90ba3ce94bb83bdf
Fbossio <felixbossio@gmail.com>
2023-09-23 15:05:24 -0500

    chore: configure postgres container for testing

--- database.js
+++ database.js
@@ -4,6 +4,12 @@ const {
   POSTGRES_DB,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
+
+  TEST_POSTGRES_HOST,
+  TEST_POSTGRES_PORT,
+  TEST_POSTGRES_DB,
+  TEST_POSTGRES_USER,
+  TEST_POSTGRES_PASSWORD,
 } = process.env;
 
 module.exports = {
@@ -14,5 +20,13 @@ module.exports = {
     database: POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
+  },
+  test: {
+    driver: 'pg',
+    host: TEST_POSTGRES_HOST,
+    port: parseInt(TEST_POSTGRES_PORT, 10),
+    database: TEST_POSTGRES_DB,
+    user: TEST_POSTGRES_USER,
+    password: TEST_POSTGRES_PASSWORD,
   },
 };
--- src/config/database.ts
+++ src/config/database.ts
@@ -2,15 +2,41 @@ import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
+const { NODE_ENV } = process.env;
+let dbConfig;
 
-const { host, port, database, user, password } = process.env;
+switch (NODE_ENV) {
+  case 'test':
+    dbConfig = {
+      host: process.env.TEST_POSTGRES_HOST,
+      port: parseInt(process.env.TEST_POSTGRES_PORT as string, 10),
+      database: process.env.TEST_POSTGRES_DB,
+      user: process.env.TEST_POSTGRES_USER,
+      password: process.env.TEST_POSTGRES_PASSWORD,
+    };
+    break;
 
-const client = new Pool({
-  host,
-  port: parseInt(port as string, 10),
-  database,
-  user,
-  password,
-});
+  case 'dev':
+    dbConfig = {
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT as string, 10),
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    };
+    break;
+
+  default: // development
+    dbConfig = {
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT as string, 10),
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    };
+    break;
+}
+
+const client = new Pool(dbConfig);
 
 export default client;
