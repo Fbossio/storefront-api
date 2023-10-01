6272ce8142dfccbd516b06d473feaa7e275d6a11
Fbossio <felixbossio@gmail.com>
2023-10-01 14:58:48 -0500

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 const app: express.Application = express();
 const address: string = '0.0.0.0:3000';
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req: Request, res: Response) {
   res.send('Hello World!');
