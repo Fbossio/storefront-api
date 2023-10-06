2f09010f2e4554254b5860e567b7f2bd7c735cfd
Fbossio <felixbossio@gmail.com>
2023-10-06 18:57:56 -0500

    refactor: add try-catch blocks to product handlers for promise rejection handling

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -5,67 +5,91 @@ import { IProduct, IUpdateProduct, ProductStore } from '../models/product';
 const store = new ProductStore();
 
 const index = async (_req: Request, res: Response) => {
-  const products = await store.index();
-  if (!products) {
-    res.status(400).json({ error: 'Products not found' });
-    return;
+  try {
+    const products = await store.index();
+    if (!products) {
+      res.status(400).json({ error: 'Products not found' });
+      return;
+    }
+    res.json(products);
+  } catch (error) {
+    throw new Error(`Error fetching products: ${error}`);
   }
-  res.json(products);
 };
 
 const show = async (req: Request, res: Response) => {
-  const product = await store.show(req.params.id);
-  if (!product) {
-    res.status(400).json({ error: 'Product not found' });
-    return;
+  try {
+    const product = await store.show(req.params.id);
+    if (!product) {
+      res.status(400).json({ error: 'Product not found' });
+      return;
+    }
+    res.json(product);
+  } catch (error) {
+    throw new Error(`Error fetching product: ${error}`);
   }
-  res.json(product);
 };
 
 const create = async (req: Request, res: Response) => {
-  const product = {
-    name: req.body.name,
-    price: parseInt(req.body.price),
-    category: req.body.category,
-  };
-  const newProduct = await store.create(product as unknown as IProduct);
-  if (!newProduct) {
-    res.status(400).json({ error: 'Product not created' });
-    return;
+  try {
+    const product = {
+      name: req.body.name,
+      price: parseInt(req.body.price),
+      category: req.body.category,
+    };
+    const newProduct = await store.create(product as unknown as IProduct);
+    if (!newProduct) {
+      res.status(400).json({ error: 'Product not created' });
+      return;
+    }
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(`Error creating product: ${error}`);
   }
-  res.json(newProduct);
 };
 
 const destroy = async (req: Request, res: Response) => {
-  const deleted = await store.delete(req.params.id);
-  if (!deleted) {
-    res.status(400).json({ error: 'Product not deleted' });
-    return;
+  try {
+    const deleted = await store.delete(req.params.id);
+    if (!deleted) {
+      res.status(400).json({ error: 'Product not deleted' });
+      return;
+    }
+    res.json(deleted);
+  } catch (error) {
+    throw new Error(`Error deleting product: ${error}`);
   }
-  res.json(deleted);
 };
 
 const update = async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const product = {
-    ...req.body,
-    id,
-  };
-  const updated = await store.update(product as unknown as IUpdateProduct);
-  if (!updated) {
-    res.status(400).json({ error: 'Product not updated' });
-    return;
+  try {
+    const id = req.params.id;
+    const product = {
+      ...req.body,
+      id,
+    };
+    const updated = await store.update(product as unknown as IUpdateProduct);
+    if (!updated) {
+      res.status(400).json({ error: 'Product not updated' });
+      return;
+    }
+    res.json(updated);
+  } catch (error) {
+    throw new Error(`Error updating product: ${error}`);
   }
-  res.json(updated);
 };
 
 const topProducts = async (_req: Request, res: Response) => {
-  const products = await store.topProducts();
-  if (!products) {
-    res.status(400).json({ error: 'Products not found' });
-    return;
+  try {
+    const products = await store.topProducts();
+    if (!products) {
+      res.status(400).json({ error: 'Products not found' });
+      return;
+    }
+    res.json(products);
+  } catch (error) {
+    throw new Error(`Error fetching products: ${error}`);
   }
-  res.json(products);
 };
 
 const productRoutes = (app: express.Application) => {
